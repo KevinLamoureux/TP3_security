445a304386d51205c15f813265acc5efbf9089c4
root <root@YuPanda.localdomain>
2021-01-04 18:53:19 +0100

    add generate and send function

--- main.py
+++ main.py
@@ -2,33 +2,43 @@ import argparse
 from src.module import generate, send, receive
 
 def arguments_parse():
-	# specif arguments
-	parser = argparse.ArgumentParser(description="Generate, send and receive message", prog="main")
+    # specif arguments
+    parser = argparse.ArgumentParser(description="Generate, send and receive message", prog="main")
 
-	parser.add_argument("dir", help="The directory that stores the pads")
-	parser.add_argument("f", nargs='?', help="File to read", type=str)
-	groupMode = parser.add_mutually_exclusive_group()
-	groupMode.add_argument("-g", "--generate", help="Change program mode(Generate)", action="store_true")
-	groupMode.add_argument("-s", "--send", help="Change program mode(Send)", action="store_true")
-	groupMode.add_argument("-r", "--receive", help="Change program mode(Receive)", action="store_true")
+    parser.add_argument("dir", help="The directory that stores the pads")
+    parser.add_argument("f", nargs='?', help="File to read", type=str)
+    groupMode = parser.add_mutually_exclusive_group()
+    groupMode.add_argument("-g", "--generate", help="Change program mode(Generate)", action="store_true")
+    groupMode.add_argument("-s", "--send", help="Change program mode(Send)", action="store_true")
+    groupMode.add_argument("-r", "--receive", help="Change program mode(Receive)", action="store_true")
 
-	group = parser.add_mutually_exclusive_group()
-	group.add_argument("-f", "--filename", help="Write the text contained in the file", type=str)
-	group.add_argument("-t", "--sometext", help="Write the text as argument", type=str)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-f", "--filename", help="Write the text contained in the file", type=str)
+    group.add_argument("-t", "--sometext", help="Write the text as argument", type=str)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	if((args.generate or args.receive) and (args.filename is not None or args.sometext is not None)):
-		parser.error("--generate or --receive not accept --filename or --sometext argument")
-	if(args.receive and args.f is None):
-		parser.error("--receive need to fileName")
-	print(args)
+    if((args.generate or args.receive) and (args.filename is not None or args.sometext is not None)):
+        parser.error("--generate or --receive not accept --filename or --sometext argument")
+    if(args.receive and args.f is None):
+        parser.error("--receive need to fileName")
+    print(args)
 
-	if(args.send):
-		send(args.dir)
-	elif(args.receive):
-		receive(args.dir)
-	else:
-		generate(args.dir)
+    if(args.send):
+        if not args.sometext and not args.filename :
+            message = input("Enter message : ")
+        elif args.filename :
+            try:
+                f = open(args.filename, 'r')
+                message = f.read()
+            except IOError:
+                sys.exit('File not accessible')
+        else:
+            message = args.sometext
+        send(args.dir, message)
+    elif(args.receive):
+        receive(args.dir)
+    else:
+        generate(args.dir)
 
 arguments_parse()
--- src/module.py
+++ src/module.py
@@ -1,8 +1,140 @@
-def generate(dirPath):
-	print("generate")
+import subprocess
+import os
+import sys
 
-def send(dirPath): 
-	print("send")
+def lengthCheck(number_byte_message):
+    ''' (integer) -> bool '''
+    number_byte_storage = 2000 * 8
+    if number_byte_message > number_byte_storage:
+        return True
+    return False
 
-def receive(dirPath):
-	print("receive")
+
+def getListRep(dir_path):
+    ''' (string) -> array<string> '''
+    list_rep=[]
+    for i in os.listdir(dir_path):
+        try :
+            if (int(i) >= 0 and int(i) < 10000):
+                if os.path.isdir(dir_path + "/" + i):
+                    list_rep.append(i)
+        except ValueError:
+            continue
+    return list_rep
+
+def createRepertory(dir_path):
+    ''' (string) -> string '''
+    # try create repertory
+    try:
+        os.makedirs(dir_path)
+    except OSError:
+        if not os.path.isdir(dir_path):
+            Raise
+
+    list_rep = getListRep(dir_path)
+    
+    #if there are directories, if not, we create directory 0000
+    if(len(list_rep) > 0):
+        name_repertory = int(list_rep[len(list_rep)-1])
+        name_repertory += 1
+        new_repertory = str(name_repertory).zfill(4)
+    else:
+        new_repertory = "0000"
+    new_path_repertory = dir_path + '/' + new_repertory
+    os.makedirs(new_path_repertory)
+    return new_path_repertory
+
+
+def generateNumber(number_bytes):
+    ''' (integer) -> binary '''
+    with open("/dev/urandom", 'rb') as f:
+        tmp = f.read(number_bytes)
+        number = ''
+        for i in range(number_bytes):
+            number += (bin(tmp[i])[2:].zfill(8))
+    return number
+
+def writeFile(path_File, content):
+    ''' (string, integer) -> none '''
+    f = open(path_File, "w")
+    f.write(str(content))
+    f.close()
+
+def generate(dir_path):
+    ''' (string) -> none '''
+    new_path_repertory = createRepertory(dir_path)
+    for i in range(0, 100):
+        path_File = new_path_repertory + '/' + str(i).zfill(2)
+        writeFile(path_File + 'p', generateNumber(48))
+        writeFile(path_File + 's', generateNumber(48))
+        writeFile(path_File + 'c', generateNumber(2000))
+    print("Generate finish")
+
+def splitPad(content):
+    '''(binary) -> array<binary> '''
+    split_pad = []
+    for i in range (0, len(content), 8):
+        split_pad.append(content[i : i + 8])
+    return split_pad
+
+def binToChar(path_repertory_current, type_file):
+    '''(string, char) -> string '''
+    f = open(path_repertory_current + type_file)
+    list_pad = splitPad(f.readlines()[0])
+    new_char = ""
+    for index_message in range(len(list_pad)):
+        # Use the mask on each character
+        new_char += chr(int(list_pad[index_message], 2))
+    f.close()
+    return new_char
+
+def send(dir_path, message):
+    '''(string, string) -> none '''
+    number_byte_message = len(message) * 8 
+    if(lengthCheck(number_byte_message)):
+        sys.exit('Error length message. Stop program')
+    if not(os.path.isdir(dir_path)):
+         sys.exit('Directory does not exist. Stop program')
+
+    list_rep = getListRep(dir_path)
+    if (len(list_rep) < 1):
+        sys.exit('No existing PAD. Stop program')
+
+    find = False
+    for rep in range(len(list_rep)):
+        new_path_repertory = dir_path + '/' + list_rep[rep]
+        for i in range(0, 100):
+            path_File = new_path_repertory + '/' + str(i).zfill(2) + 'c'
+            if(os.path.exists(path_File)):
+                path_pad = path_File
+                path_repertory_current = new_path_repertory + '/' + str(i).zfill(2)
+                #if the file does not comply, we go to the next one
+                try:
+                    suffix = binToChar(path_repertory_current, "p")
+                    f = open(path_pad)
+                    msg = ""
+                    list_pad = splitPad(f.readlines()[0])
+                    for index_message in range(len(message)):
+                        # Use the mask on each character
+                        msg += chr(ord(message[index_message]) + int(list_pad[index_message], 2))
+                    f.close()
+                    prefix = binToChar(path_repertory_current, "s")
+                    #replaced to construct the file name
+                    path_message = path_repertory_current.replace('/', '-')
+                    f = open(path_message + 't' , "w")
+                    f.write(suffix + msg + prefix)
+                    f.close()
+                    #shred file p
+                    subprocess.Popen(['shred', path_pad])
+                    find = True
+                    break;
+                except:
+                    continue
+        if(find):
+            break
+    if not(find):
+        sys.exit('No existing PAD. Stop program')
+
+
+def receive(dir_path):
+    print("receive")
